1f4f4b799ab3397392f08a84d36c49622111ba0f
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-06-10 15:34:36 +0700

    initial SEQK files

---
+++ Ngay 2020-06-03/SEQK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+const int MOD = 10000000007;
+
+int k, m, n;
+
+int32_t main(void) {
+    FastIO;
+    freopen("SEQK.INP","r",stdin);
+    freopen("SEQK.OUT","w",stdout);
+    cin >> k >> m >> n;
+
+    return 0;
+}
