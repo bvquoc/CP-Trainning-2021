4fb614874403699cfc06e112011718c0505e6aa7
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-06-30 09:58:11 +0700

    solve pig problem (6/20)

--- Ngay 2020-06-29/pig.cpp
+++ Ngay 2020-06-29/pig.cpp
@@ -7,20 +7,48 @@ int n, m, m_max = 0;
 int res = 0;
 vector <int> a, c[N];
 
-int f[10000007];
-void Sub1_2() {
+struct MaxST{
+	vector <int> st, gt;
+	MaxST() {}
+	MaxST(int n) {
+		st.resize((4*(n+5)));
+		gt.resize(n+1);
+	}
+    void assign (int n) {
+		st.resize((4*(n+5)));
+		gt.resize(n+1);
+	}
+	void update(int id, int l, int r, int i, int v) {
+	    if (i < l || r < i) return;
+	    if (l==r) {
+	        st[id]=v;
+	        gt[i]=v;
+	        return;
+	    }
+	    int mid = (l+r)>>1, id1=id<<1, id2=id1+1;
+	    update(id1,l,mid,i,v);
+	    update(id2,mid+1,r,i,v);
+	    st[id]=max(st[id1],st[id2]);
+	}
+	int getmax(int id, int l, int r, int u, int v) {
+	    if (v < l || r < u) return INT_MIN;
+	    if (u <= l && r <= v) return st[id];
+	    int mid = (l+r)>>1, id1=id<<1, id2=id1+1;
+	    return max(getmax(id<<1,l,mid,u,v), getmax(id2, mid+1, r, u, v));
+	}
+};
 
+MaxST T;
+
+void Sub1_2() {
+    int T_size = *max_element(a.begin(),a.end());
+    T.assign(T_size);
     // a[] = { 1,2,2,1,2,2 } -> 9
     for (int i=1; i<=m; i++) {
-        for (int j=0; j<=a[i]; j++) {
-            f[a[i]] = max(f[a[i]], f[j]+a[i]);
-        }
-        cerr << f[a[i]] << ' ';
+        T.update(1, 0, T_size,a[i], T.getmax(1,0,T_size,0,a[i]) + a[i]);
     }
     
-    for (int i=1; i<=m; i++) {
-        res = max(res, f[a[i]]);
-    }
+    res = T.getmax(1, 0, T_size, 0, T_size);
 }
 
 int32_t main(void) {
@@ -59,28 +87,3 @@ int32_t main(void) {
     cout << res;
     return 0;
 }
-
-
-// int z[30];
-// void update() {
-//     int cur_sum = 0;
-//     for (int i=1;i<=m;i++) {
-//         if (z[i]==1) cur_sum += a[i];
-//     }
-//     res = max(cur_sum, res);
-// }
-// void Try(int i, int cur) {
-//     for (int j=0;j<=1;j++) {
-//         if (j==0 || a[i]>=cur) {
-//             z[i] = j;
-//             if (i==m) update();
-//             else {
-//                 int tmp = 0;
-//                 if (j == 1) {
-//                     tmp = a[i];
-//                 }
-//                 Try(i+1, tmp);
-//             }
-//         }
-//     }
-// }
