25c06801c1365a177d7be011144b8d068c461da5
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-07-02 00:06:17 +0700

    add problem link

--- Ngay 2020-06-29/pig.cpp
+++ Ngay 2020-06-29/pig.cpp
@@ -1,4 +1,4 @@
-/* http://lqdoj.edu.vn/problem/goldbach */
+/* http://lqdoj.edu.vn/problem/pig */
 #include <bits/stdc++.h>
 #define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
