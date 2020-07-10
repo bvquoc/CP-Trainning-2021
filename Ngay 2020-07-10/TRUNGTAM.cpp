8284db58fabe56e0b42ec6aac814898926dd4408
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-07-10 09:49:07 +0700

    solve TRUNGTAM problem

--- Ngay 2020-07-10/TRUNGTAM.cpp
+++ Ngay 2020-07-10/TRUNGTAM.cpp
@@ -1,46 +1,61 @@
 #include <bits/stdc++.h>
 #define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
+#define int long long
 
 using ii = pair <int,int>;
-const int N = 102, INF = INT_MAX;
-int n, m, d[N][N];
+const int N = 1003, INF = 9000000000000000000;
+int n, m, d[N], a[N][N];
 int ans = INF, ans_id = 1;
+vector <int> adj[N];
+
+void dijkstra(int _u) {
+    priority_queue<ii, vector<ii>, greater<ii>> pq;
+    for (int i=1; i<=n; i++) {
+        d[i] = INF;
+    }
+    d[_u] = 0;
+    pq.push(ii(0, _u));
+
+    while (pq.size()) {
+        int u = pq.top().second;
+        int du = pq.top().first;
+        pq.pop();
+        if (du != d[u]) continue;
+
+        for (int i = 0; i < adj[u].size(); i++) {
+            int v = adj[u][i];
+            int uv = a[u][v];
+            if (d[v] > du + uv) {
+                d[v] = du + uv;
+                pq.push(ii(d[v], v));
+            }
+        }
+    }
+}
 
 int32_t main(void) {
     FastIO;
     freopen("TRUNGTAM.INP","r",stdin);
     freopen("TRUNGTAM.OUT","w",stdout);
     cin >> n >> m;
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
-            d[i][j] = INF;
-            if (i==j) d[i][j] = 0;
-        }
-    }
     for (int i=1; i<=m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
-        d[u][v] = w;
-        d[v][u] = w;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        a[u][v] = w;
+        a[v][u] = w;
     }
 
-    // Floyd
-    for (int k=1; k<=n; k++) {
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=n; j++) {
-                if (d[i][j] > d[i][k] + d[k][j]) {
-                    d[i][j] = d[i][k] + d[k][j];
-                }
-            }
-        }
-    }
-    
     // Solve
     for (int i=1;i<=n;i++) {
         int cur = 0;
+        dijkstra(i);
         for (int j=1; j<=n; j++) {
-            cur += d[i][j];
+            if (d[j]!=INF) {
+                cur += d[j];
+            }
         }
         if (ans > cur) {
             ans = cur;
