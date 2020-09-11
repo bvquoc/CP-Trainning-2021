0df79c0530097563e7a17dbdfdeef7ad228a94a9
bvquoc <bvquoc@pop-os.localdomain>
2020-09-11 15:18:35 +0700

    solve DEMSO (90/100)

---
+++ Ngay 2020-09-11/DEMSO.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define FORD(i, a, b) for(int i=a; i>=b; i--)
+#define endl '\n'
+using namespace std;
+
+using ii = pair <int, int>;
+using ld = long double;
+
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
+}
+
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+int n, x;
+set <int> S;
+
+signed main(void) {
+    FastIO;
+    freopen("DEMSO.INP","r",stdin);
+    freopen("DEMSO.OUT","w",stdout);
+    Read(n);
+    FOR(i,1,n) {
+        Read(x);
+        S.insert(x);
+    }
+
+    int res = n - S.size();
+    Write(res);
+    return 0;
+}
