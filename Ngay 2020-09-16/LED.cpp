2b59be2ee69219cbc67db4917aaf97ac48d6ffd2
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-17 08:22:34 +0700

    add LED code template

---
+++ Ngay 2020-09-16/LED.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define FORD(i, a, b) for(int i=a; i>=b; i--)
+#define endl '\n'
+// #define int long long
+using namespace std;
+
+using ii = pair <int, int>;
+using ld = long double;
+
+int n, k;
+
+signed main(void) {
+    FastIO;
+    freopen("LED.INP","r",stdin);
+    freopen("LED.OUT","w",stdout);
+    cin >> n >> k;
+    return 0;
+}
