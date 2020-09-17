da070d53c7cd9964bb5d5c4a8a5d97b986b7a97e
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-17 08:49:49 +0700

    solve NGUYENTO (AC)

--- Ngay 2020-09-11/NGUYENTO.cpp
+++ Ngay 2020-09-11/NGUYENTO.cpp
@@ -1,74 +1,69 @@
 #include <bits/stdc++.h>
-#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-#define fi first
-#define se second
-#define FOR(i, a, b) for(int i=a; i<=b; i++)
-#define FORD(i, a, b) for(int i=a; i>=b; i--)
-#define endl '\n'
-// #define int long long
+#define FastIO ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
-using ii = pair <int, int>;
-using ld = long double;
+string s, nt[1005];
+int mt[1005];
+int res = 0;
+struct data {
+    int l, r, k;
+} A[1005];
 
-const int N = 10004;
-string s;
-vector <string> a;
-vector <int> res;
-bool isPrime[N+1];
-bool mark[N+1];
-
-void sieve() {
-    for(int i = 0; i < N;++i) {
-        isPrime[i] = true;
+int t = 0;
+void Nguyen_to() {
+    int N = 1000;
+    bool check[N + 1];
+    for (int i = 2; i <= N; i++) {
+        check[i] = true;
     }
-    isPrime[0] = isPrime[1] = false;
-    for(int i = 2; i * i < N; ++i) {
-        if(isPrime[i] == true) {
-            for(int j = i * i; j < N; j += i)
-                isPrime[j] = false;
+
+    for (int i = 2; i <= N; i++) {
+        if (check[i]) {
+            for (int j = 2 * i; j <= N; j += i) {
+                check[j] = false;
+            }
         }
     }
-}
 
-void cut() {
-    int i = 0;
-    while (i < s.size()) {
-        if (!('0'<=s[i] && s[i]<='9') && i<s.size()-1) i++;
-        if (!('0'<=s[i] && s[i]<='9')) continue;
-        int j = i;
-        while ('0'<=s[j] && s[j]<='9' && j<s.size()) j++;
-        a.push_back(s.substr(i,j-i));
-        i = j;
-    }
-}
-
-void getAns(string x) {
-    FOR(i,0,x.size()-1) {
-        int cur = 0;
-        FOR(j,i,x.size()-1) {
-            cur *= 10;
-            cur += (x[j]-'0');
-            if (isPrime[cur] && !mark[cur]) {
-                res.push_back(cur);
-                mark[cur] = true;
+    check[601] = false;
+    for (int i = 2; i <= N; i++) {
+        if (check[i] == true) {
+            t++;
+            int k = i;
+            while (k != 0) {
+                int m = k % 10;
+                nt[t] = char(m + '0') + nt[t];
+                mt[t]++;
+                k /= 10;
             }
         }
     }
 }
 
-signed main(void) {
-    FastIO;
-    freopen("NGUYENTO.INP","r",stdin);
-    freopen("NGUYENTO.OUT","w",stdout);
-    cin >> s;
-    cut();
-    sieve();
-    for (string x: a) {
-        getAns(x);
-    }
 
-    cout << res.size() << endl;
-    for (int x: res) cout << x << ' ';
+signed main() {
+    FastIO;
+    freopen("NGUYENTO.INP", "r", stdin);
+    freopen("NGUYENTO.OUT", "w", stdout);
+    cin >> s;
+    Nguyen_to();
+    for (int i = 1; i <= t; i++) {
+        int k = s.find(nt[i]);
+        if (k != -1) {
+            res++;
+            A[res].r = i;
+            A[res].l = k;
+            A[res].k = mt[i];
+        }
+    }
+    cout << res << '\n';
+    sort(A + 1, A + res + 1, [](const data &x, const data &y) {
+        return (x.l < y.l) || (x.k < y.k && x.l == y.l);
+    });
+    
+    for (int i = 1; i <= res; i++) {
+        int k = A[i].r;
+        cout << nt[k] << ' ';
+    }
     return 0;
 }
