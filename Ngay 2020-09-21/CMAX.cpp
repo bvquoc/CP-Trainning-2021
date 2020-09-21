7953c73e1f371b4ecb386be1cb09be67ca897418
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-21 14:28:50 +0700

    solve CMAX (AC)

--- Ngay 2020-09-21/CMAX.cpp
+++ Ngay 2020-09-21/CMAX.cpp
@@ -5,30 +5,55 @@
 #define FOR(i, a, b) for(int i=a; i<=b; i++)
 #define FORD(i, a, b) for(int i=a; i>=b; i--)
 #define endl '\n'
-#define int long long
 using namespace std;
 
 using ii = pair <int, int>;
 using ld = long double;
 
-const int N = 100005, INF = LLONG_MAX;
-int n; ii a[N];
+const int N = 100005, INF = INT_MAX;
+int n, m; ii L[N];
 
+double x[N]; // Mảng tọa độ các giao điểm
+int p[N]; // Hệ số góc của các đoạn gấp khúc
+int q[N]; // Hằng số của đoạn gấp khúc
+
+bool ok (int i) {
+    int u = m;
+    if (L[i].fi==L[u].fi) return false;
+    if (m==1) return true;
+    double xM = 1.0 * (L[u].se - L[i].se) / (L[i].fi - L[u].fi);
+    u = m-1;
+    double xN = 1.0 * (L[u].se - L[i].se) / (L[i].fi - L[u].fi);
+    return (xM > xN);
+}
 signed main(void) {
     FastIO;
     freopen("CMAX.INP","r",stdin);
     freopen("CMAX.OUT","w",stdout);
     cin >> n;
-    FOR(i,1,n) cin >> a[i].first >> a[i].second;
-    int q, x, ans; 
+    FOR(i,1,n) cin >> L[i].first >> L[i].second;
+
+    sort(L+1,L+1+n);
+    m = 0;
+    for (int i = 1; i <= n; ++i) {
+        while (m > 0 && !ok (i)) --m;
+        L[++m] = L[i];
+    }
+
+    x[0] = -INF, p[0] = L[1].fi, q[0] = L[1].se;
+    for (int i = 1; i < m; ++i) {
+        x[i] = 1.0 * (L[i].se - L[i+1].se) / (L[i+1].fi - L[i].fi);
+        p[i] = L[i+1].fi;
+        q[i] = L[i+1].se;
+    }
+    x[m]=INF;
+
+    int q, z; 
     cin >> q;
     while (q--) {
-        cin >> x;
-        ans = -INF;
-        FOR(i,1,n) {
-            ans = max(ans,a[i].first*x + a[i].second);
-        }
-        cout << ans << endl;
+        cin >> z;
+        int u = upper_bound (x, x + m, z) - x - 1;
+        cout << p[u]*z + ::q[u] << endl;
     }
     return 0;
 }
