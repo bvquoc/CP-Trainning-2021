e952b59249236c89ac3abc11080f120e0e5edb5a
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-24 09:51:23 +0700

    solve SREC (AC)

--- Ngay 2020-09-21/SREC.cpp
+++ Ngay 2020-09-21/SREC.cpp
@@ -1,56 +1,81 @@
 #include <bits/stdc++.h>
-#define maxn 1000005
-#define ft first
-#define sc second
-#define oo 1.0e18
+#define FastIO ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define FORD(i, a, b) for(int i=a; i>=b; i--)
+#define endl '\n'
 
 using namespace std;
-typedef pair<int, int> II;
-typedef pair<II, int64_t> III;
 
-int n;
-III a[maxn];
-int64_t dp[maxn];
-int k;
-int64_t p[maxn], q[maxn];
-long double x[maxn];
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
+}
+
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+using ll = long long;
+using ld = long double;
+using ii = pair<int, int>;
+using iii = pair<ii, ll>;
+
+const int N = 1000005;
+const ll INF = 1e18;
+
+int n, k;
+iii a[N];
+ll dp[N], p[N], q[N];
+ld x[N];
 
 bool ok (int i) {
-    int64_t P = a[i].ft.ft;
-    int64_t Q = dp[i];
-    long double xm = (long double) (Q - q[k]) / (p[k] - P);
-    long double xn = (long double) (Q - q[k - 1]) / (p[k - 1] - P);
+    ll P = a[i].fi.fi;
+    ll Q = dp[i];
+    ld xm = (ld) (Q - q[k]) / (p[k] - P);
+    ld xn = (ld) (Q - q[k - 1]) / (p[k - 1] - P);
     return (xn < xm);
 }
 
 int main() {
-    freopen("SREC.INP", "r", stdin);
-    freopen("SREC.OUT", "w", stdout);
+    FastIO;
+    freopen ("SREC.INP", "r", stdin);
+    freopen ("SREC.OUT", "w", stdout);
 
-    scanf ("%d", &n);
-    for (int i = 1; i <= n; ++i) {
-        int u, v;
-        int64_t w;
-        scanf ("%d %d %I64d", &u, &v, &w);
+    Read(n);
+    FOR(i,1,n) {
+        int u, v; ll w;
+        Read(u); Read(v); Read(w);
         a[i] = {{u, v}, w};
     }
+
     sort (a + 1, a + n + 1);
     dp[0] = 0;
     k = 0;
-    x[0] = -oo, p[0] = a[0].ft.ft, q[0] = dp[0];
+    x[0] = -INF, p[0] = a[0].fi.fi, q[0] = dp[0];
     int u = 0;
-    for (int i = 1; i <= n; ++i) {
-        while (u <= k && x[u] <= -a[i].ft.sc)
+    FOR(i,1,n) {
+        while (u <= k && x[u] <= -a[i].fi.se)
             ++u;
         --u;
-        dp[i] = 1ll * a[i].ft.ft * a[i].ft.sc - a[i].sc + q[u] + p[u] * (-a[i].ft.sc);
+        dp[i] = 1ll * a[i].fi.fi * a[i].fi.se - a[i].se + q[u] + p[u] * (-a[i].fi.se);
         while (k > 0 && !ok (i))
             --k;
-        p[++k] = a[i].ft.ft, q[k] = dp[i];
-        x[k] = (long double) (q[k] - q[k - 1]) / (p[k - 1] - p[k]);
+        p[++k] = a[i].fi.fi, q[k] = dp[i];
+        x[k] = (ld) (q[k] - q[k - 1]) / (p[k - 1] - p[k]);
     }
-    int64_t res = dp[1];
-    for (int i = 2; i <= n; ++i)
-        res = max (res, dp[i]);
-    printf ("%I64d", res);
+    ll res = *max_element(dp+1,dp+1+n);
+    Write(res);
 }
