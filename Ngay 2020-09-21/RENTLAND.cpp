74b9c6443267fc8e7156d212bf839fd493a0b496
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-24 10:15:49 +0700

    tmp file RENTLAND

--- Ngay 2020-09-21/RENTLAND.cpp
+++ Ngay 2020-09-21/RENTLAND.cpp
@@ -12,44 +12,49 @@ using ii = pair <int, int>;
 using ld = long double;
 
 const int N = 50004, INF = LLONG_MAX;
-int n; ii a[N];
-int M[N][65];
-vector < vector<int> > f;
 
-void RMQ() {
-    FOR(i,1,n) M[i][0] = a[i].second;
-    for (int k=1; (1<<k)<=n; k++)
-        for (int i=1; i+(1<<k)-1<=n; i++)
-            M[i][k] = max(M[i][k-1], M[i+(1<<(k-1))][k-1]);
-}
-int getmax(int u, int v) {
-    int k = log2(v-u+1);
-    return max(M[u][k], M[v-(1<<k)+1][k]);
-}
-
-int dp(int l, int r) {
-    if (f[l][r] != INF) return f[l][r];
-    int cur = a[r].first*getmax(l,r);
-    if (l == r) return a[l].first*a[l].second;
-    FOR(j,l,r-1) {
-        cur = min(cur,dp(l,j)+dp(j+1,r));
-    }
-    return f[l][r] = cur;
-}
-
-void init_DP() {
-    f.assign(n+2,vector<int> (n+2,INF));
-}
+int n, w[N], l[N];
+int dp[N];
 
 signed main(void) {
     FastIO;
     freopen("RENTLAND.INP","r",stdin);
     freopen("RENTLAND.OUT","w",stdout);
     cin >> n;
-    FOR(i,1,n) cin >> a[i].first >> a[i].second;
-    sort(a+1,a+1+n);
-    RMQ();
-    init_DP();
-    cout << dp(1,n);
+    { // Read Input
+        ii a[N];
+        FOR(i,1,n) {
+            cin >> w[i] >> l[i];
+            a[i] = {w[i],l[i]};
+        }
+        sort(a+1,a+1+n);
+        FOR(i,1,n) {
+            w[i] = a[i].first;
+            l[i] = a[i].second;
+        }
+
+        bool d[N];
+        int wmax = w[n];
+        FORD(i,n-1,1) {
+            if (w[i] <= wmax) d[i] = 1;
+            else wmax = w[i];
+        }
+        int n1 = 0;
+        FOR(i,1,n) {
+            if (!d[i])
+                l[++n1] = l[i], w[n1] = w[i];
+        }
+        n = n1;
+    }
+    FOR(i,1,n) cout << w[i] << ' ' << l[i] << endl;
+    
+    dp[0] = 0;
+    FOR(i,1,n) {
+
+        FOR(j,0,i-1) dp[i] = min(dp[i],dp[j]+w[j+1]*l[i]);
+        cout << dp[i] << ' ';
+    }
+    cout << dp[n];
+    
     return 0;
 }
