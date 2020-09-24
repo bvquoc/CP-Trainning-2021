94f8b977c84936b1daf380df8868e9d4159c1aa3
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-24 08:52:29 +0700

    solve AC (add fastIO)

--- Ngay 2020-09-21/CWALKWAY.cpp
+++ Ngay 2020-09-21/CWALKWAY.cpp
@@ -6,6 +6,26 @@
 
 using namespace std;
 
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
+}
+
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
 using ll = long long;
 using ld = long double;
 
@@ -30,9 +50,9 @@ signed main() {
     FastIO;
     freopen("CWALKWAY.INP", "r", stdin);
     freopen("CWALKWAY.OUT", "w", stdout);
-    cin >> n >> c;
+    Read(n); Read(c);
     a[0] = 0;
-    FOR(i,1,n) cin >> a[i];
+    FOR(i,1,n) Read(a[i]);
 
     dp[0] = 0;
     k = 0, x[0] = -INF, p[0] = -2LL*a[1], q[0] = dp[0] + 1LL*a[1]*a[1];
@@ -45,6 +65,6 @@ signed main() {
         p[++k] = -2LL*a[i+1], q[k] = dp[i] + 1LL*a[i+1]*a[i+1];
         x[k] = 1.0*(q[k]-q[k-1]) / (p[k-1]-p[k]);
     }
-    cout << dp[n];
+    Write(dp[n]);
     return 0;
 }
