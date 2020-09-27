916d96c9f9fab3113510000be94e509705c4f02a
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-09-27 10:07:18 +0700

    solve COLORING

--- Ngay 2020-09-27/COLORING.cpp
+++ Ngay 2020-09-27/COLORING.cpp
@@ -10,29 +10,34 @@ using namespace std;
 using ii = pair <int, int>;
 using ld = long double;
 
-const int MN = 1005;
-int m, n, a[MN][MN], X, Y, res;
-bool cx[MN][MN];
-set <int> d;
-int MaxLT = 0, tmp = 0;
+int dx[] = {0, 0, 1, 0,-1};
+int dy[] = {0, 1, 0,-1, 0};
 
-void Loang(int x, int y) {
-    if (!cx[x][y]) return;
-    tmp++;
-    cx[x][y] = false;
-    if (a[x][y] == a[x+1][y]) Loang(x+1,y);
-    if (a[x][y] == a[x][y+1]) Loang(x,y+1);
-    if (a[x][y] == a[x-1][y]) Loang(x-1,y);
-    if (a[x][y] == a[x][y-1]) Loang(x,y-1);
+const int MN = 1005;
+int m, n, a[MN][MN];
+bool mark[MN][MN];
+int res = 0;
+set <int> ke[1000006];
+set <int> col;
+
+bool isValid(int x, int y) {
+    if (x<1 || x>m) return false;
+    if (y<1 || y>n) return false;
+    if (mark[x][y]) return false;
+    return true;
 }
-void DFS(int x, int y, int k) {
-    if (!cx[x][y]) return;
-    tmp++;
-    cx[x][y] = false;
-    if (a[X][Y] == a[x+1][y] || a[x+1][y] == k) DFS(x+1,y,k);
-    if (a[X][Y] == a[x][y+1] || a[x][y+1] == k) DFS(x,y+1,k);
-    if (a[X][Y] == a[x-1][y] || a[x-1][y] == k) DFS(x-1,y,k);
-    if (a[X][Y] == a[x][y-1] || a[x][y-1] == k) DFS(x,y-1,k);
+
+int cur; 
+void DFS(int x, int y, int col1, int col2) {
+    cur++;
+    mark[x][y] = true;
+    FOR(i,1,4) {
+        int _x = x + dx[i], _y = y + dy[i];
+        if (!isValid(_x,_y)) continue;
+        if (a[_x][_y] == col1 || a[_x][_y] == col2) {
+            DFS(_x,_y, col1, col2);
+        }
+    }
 }
 
 int32_t main() {
@@ -42,28 +47,30 @@ int32_t main() {
     cin >> m >> n;
     FOR(i,1,m) FOR(j,1,n) {
         cin >> a[i][j];
-        d.insert(a[i][j]);
+        col.insert(a[i][j]);
     }
 
-    FOR(i,1,m) FOR(j,1,n) cx[i][j] = true;
     FOR(i,1,m) FOR(j,1,n) {
-        if (cx[i][j]) {
-            tmp = 0;
-            Loang(i,j);
-            if (MaxLT < tmp) {
-                X = i; Y = j;
-                MaxLT = tmp;
+        FOR(k,1,2) {
+            if (isValid(i+dx[k],j+dy[k] && a[i][j]<a[i+dx[k]][j+dy[k]])) {
+                ke[a[i][j]].insert(a[i+dx[k]][j+dy[k]]);
             }
         }
     }
-
-    res = MaxLT; d.erase(a[X][Y]);
-    for (set <int>::iterator it = d.begin(); it != d.end(); ++it) {
-        FOR(i,1,m) FOR(j,1,n) cx[i][j] = true;
-        tmp = 0;
-        DFS(X,Y,*it);
-        res = max(res,tmp);
+    
+    for(int col1: col) {
+        for(int col2: ke[col1]) {
+            
+            FOR(i,1,m) FOR(j,1,n) mark[i][j] = 0;
+            FOR(i,1,m) FOR(j,1,n) 
+                if (!mark[i][j] && (a[i][j]==col1 || a[i][j]==col2)) {
+                    cur = 0;
+                    DFS(i, j, col1, col2);
+                    res = max(res, cur);
+                }
+        }
     }
+
     cout << res;
     return 0;
 }
