64f16887b5b1b67ae6f05cfc91871d622b3b8a8a
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-10-13 09:58:05 +0700

    WALKING

--- Ngay 2020-10-13/WALKING.cpp
+++ Ngay 2020-10-13/WALKING.cpp
@@ -23,32 +23,33 @@ struct MaxST{
 	}
 
 	void update(int id, int l, int r, int i, int v) {
-	    if (i<l || r<i) return;
-	    if (l==r) {
-	        st[id] = v;
-	        return;
-	    }
-	    int mid = (l+r)>>1, id1 = id<<1, id2 = id1+1;
-	    update(id1,l,mid,i,v);
-	    update(id2,mid+1,r,i,v);
-	    st[id] = max(st[id1],st[id2]);
+		if (i<l || r<i) return;
+		if (l==r) {
+			st[id] = v;
+			return;
+		}
+		int mid = (l+r)>>1, id1 = id<<1, id2 = id1+1;
+		update(id1,l,mid,i,v);
+		update(id2,mid+1,r,i,v);
+		st[id] = max(st[id1],st[id2]);
 	}
 
 	int getmax(int id, int l, int r, int u, int v) {
-	    if (v<l || r<u) return INT_MIN;
-	    if (u<=l && r<=v) return st[id];
-	    int mid = (l+r)>>1, id1 = id<<1, id2 = id1+1;
-	    return max(getmax(id1,l,mid,u,v), getmax(id2, mid+1, r, u, v));
+		if (v<l || r<u) return INT_MIN;
+		if (u<=l && r<=v) return st[id];
+		int mid = (l+r)>>1, id1 = id<<1, id2 = id1+1;
+		return max(getmax(id1,l,mid,u,v), getmax(id2, mid+1, r, u, v));
 	}
 } ST;
 
 const int N = 100005;
 int n, L; ii res[N];
-struct data { 
+struct Data { 
     int id, time, speed; 
+	bool operator == (const Data &B) {
+		return time == B.time;
+	}
 } a[N];
-
-
 
 signed main(void) {
     FastIO;
@@ -59,7 +60,7 @@ signed main(void) {
         a[i].id = i;
         cin >> a[i].time >> a[i].speed;
     }
-    sort(a+1,a+1+n,[](const data &A, const data &B) {
+    sort(a+1,a+1+n,[](const Data &A, const Data &B) {
         return (A.time < B.time || (A.time == B.time && A.speed > B.speed));
     });
 
@@ -68,9 +69,9 @@ signed main(void) {
         ST.update(1,1,n,i,a[i].speed);
         res[i].first = a[i].id;
     }
-    
+	
     FOR(i,1,n) {
-
+		cout << a[i].time << ' ' << a[i].speed << endl;
     }
 
     return 0;
