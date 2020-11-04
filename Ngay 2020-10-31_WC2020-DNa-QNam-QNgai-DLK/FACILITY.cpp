0c94948e842e4efe453340a4f122277c972a8b15
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-11-04 10:45:43 +0700

    solve FACILITY (TLE - 80/100)

--- Ngay 2020-10-31_WC2020-DNa-QNam-QNgai-DLK/FACILITY.cpp
+++ Ngay 2020-10-31_WC2020-DNa-QNam-QNgai-DLK/FACILITY.cpp
@@ -30,37 +30,103 @@ bool maximize(X &x, const Y &y) {
     } else return false;
 }
 
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
+}
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
 using ii = pair <int, int>;
 using ll = long long;
 using ld = long double;
 
 const int N = 1000006;
-int n, K; 
+int n, K, res = 0; 
 struct data { 
     int s, d, r; 
-    bool operator < (const data &b) {
-        return s < b.s || (s == b.s && d == b.d && r > b.r);
-    }
+    bool operator < (const data &b) { return d < b.d; }
 } a[N];
+
+pair<int, ii> save[N]; int sz = 0;
+void compress(){
+	FOR(i, 1, n) {
+        save[++sz] = {a[i].s, {i, 1}};
+		save[++sz] = {a[i].d, {i, 2}};
+    }
+
+	sort(save + 1, save + 1 + sz);
+
+	int tmp = 0;
+	FOR(i, 1, sz){
+		if (i == 1 || save[i].fi != save[i - 1].fi) tmp++;
+		if (save[i].se.se == 1) a[save[i].se.fi].s = tmp;
+		else a[save[i].se.fi].d = tmp;
+	}
+}
+
+int bit[N];
+void update(int idx, int num){
+	while (idx < N) {
+        bit[idx] = max(bit[idx], num);
+        idx += idx & -idx;
+    }
+}
+int get(int idx){
+	int ans = 0;
+	while (idx) {
+        maximize(ans, bit[idx]);
+        idx -= idx & -idx;
+    }
+	return ans;
+}
 
 signed main(void) {
     FastIO;
     freopen("FACILITY.INP","r",stdin);
     freopen("FACILITY.OUT","w",stdout);
-    cin >> n >> K;
-    REP(i,n) cin >> a[i].s >> a[i].d >> a[i].r;
-    sort(a, a+n);
-    int res = 0;
-    REP(state,MASK(n)) {
-        int cur_cost = 0, cur_time = 0;
-        REP(i,n) if (BIT(state, i)) {
-            if (a[i].s >= cur_time) {
-                cur_cost += a[i].r;
-                cur_time = a[i].s + a[i].d + K;
-            }
-        }
-        maximize(res, cur_cost);
+    Read(n); Read(K);
+    FOR(i,1,n) {
+        Read(a[i].s); Read(a[i].d); Read(a[i].r);
+        a[i].d += a[i].s + K;
     }
-    cout << res;
+    
+    if (n <= 10) {
+        sort(a+1, a+1+n);
+        REP(state,MASK(n)) {
+            int cur_cost = 0, cur_time = 0;
+            FOR(i,1,n) if (BIT(state, i-1)) {
+                if (a[i].s >= cur_time) {
+                    cur_cost += a[i].r;
+                    cur_time = a[i].d;
+                }
+            }
+            maximize(res, cur_cost);
+        }
+        Write(res);
+        exit(0);
+    }
+
+    compress();
+    sort(a+1, a+1+n);
+    FOR(i,1,n) {
+		int st = a[i].s, en = a[i].d, c = a[i].r;
+		int cost = c + get(st);
+        res = max(res, cost);
+		update(en, cost);
+	}
+    Write(res);
     return 0;
 }
