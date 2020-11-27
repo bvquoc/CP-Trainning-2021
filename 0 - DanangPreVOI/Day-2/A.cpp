d6c7ea825654c2abc80e63a8f710fe22b818e976
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-11-27 08:54:54 +0700

    solve A - DanangPreVOI 2

--- 0 - DanangPreVOI/Day-2/A.cpp
+++ 0 - DanangPreVOI/Day-2/A.cpp
@@ -1,78 +1,19 @@
 /* 𝙰𝚞𝚝𝚑𝚘𝚛: 𝙱𝚞𝚒 𝚅𝚒 𝚀𝚞𝚘𝚌 */
 #include <bits/stdc++.h>
 #define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-#define fi first
-#define se second
-#define FOR(i, a, b) for(int i=(a); i<=(b); i++)
-#define FORD(i, a, b) for(int i=(a); i>=(b); i--)
-#define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
-#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
-#define ALL(v) (v).begin(), (v).end()
-#define BIT(x, i) (((x) >> (i)) & 1)
-#define MASK(i) (1LL << (i))
-#define testBit(n, bit) (((n) >> (bit)) & 1LL)
-#define flipBit(n, bit) ((n) ^ (1LL << (bit)))
-#define cntBit(n) __builtin_popcountll(n)
 #define SPACE putchar(' ')
-#define sqr(x) ((x)*(x))
 #define endl '\n'
 // #define int long long
 using namespace std;
-
-template <typename T>
-inline void Read(T& x) {
-    bool Neg = false;
-    char c;
-    for (c = getchar(); c < '0' || c > '9'; c = getchar())
-        if (c == '-') Neg = !Neg;
-    x = c - '0';
-    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
-        x = x * 10 + c - '0';
-    if (Neg) x = -x;
-}
-template <typename T>
-inline void Write(T x) {
-    if (x < 0) { putchar('-'); x = -x; }
-    T p = 1;
-    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
-    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
-}
-
-template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
-template<class X, class Y>
-bool minimize(X &x, const Y &y) {
-    X eps = 1e-9;
-    if (x > y + eps) {
-        x = y;
-        return true;
-    }
-    return false;
-}
-template<class X, class Y>
-bool maximize(X &x, const Y &y) {
-    X eps = 1e-9;
-    if (x + eps < y) {
-        x = y;
-        return true;
-    }
-    return false;
-}
-
-using ii = pair <int, int>;
-using ll = long long;
-using ld = long double;
-
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = { 0, 1, 0,-1};
 
 /*
 (\_/)
 ( •_•)
 / >?? */
 
-const int N = 400005;
-int n, a[N];
-set <int> S;
+int n;
+vector <int> a, c;
+vector <bool> b;
 
 // #define FILE_IO
 signed main(void) {
@@ -81,22 +22,20 @@ signed main(void) {
     freopen("A.INP","r",stdin);
     freopen("A.OUT","w",stdout);
     #endif
-    int T; Read(T);
-    Read(n);
-    FOR(i,1,n) {
-        S.insert(i);
-        Read(a[i]);
-    }
+    cin >> n >> n;
+    a.resize(n+1); b.resize(n+1);
+    int i;
+    for (i = 1; i <= n; i++) cin >> a[i];
 
-    FOR(i,1,n) if (a[i] != -1) S.erase(a[i]);
-    FOR(i,1,n) if (a[i] == -1) {
-        a[i] = *S.begin();
-        S.erase(S.begin());
-    }
+    for (i = 1; i <= n; i++) if (a[i] != -1) b[a[i]] = 1;
 
-    FOR(i,1,n) {
-        Write(a[i]);
-        SPACE;
-    }
+    int sz = 0;
+    for (i = 1; i <= n; i++) if (!b[i]) c.push_back(i);
+    int j = 0;
+    for (i = 1; i <= n; i++) if (a[i] == -1) a[i] = c[j++];
+
+    // cout << sizeof a + sizeof b + sizeof n + sizeof i + sizeof c + sizeof N + sizeof j + sizeof sz  << endl;
+
+    for (i = 1; i <= n; i++) cout << a[i] << ' ';
     return 0;
 }
