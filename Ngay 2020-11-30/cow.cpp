8c7099c5cc026da8d8d65be6d45b99d7080fd93d
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-11-30 10:51:45 +0700

    solve cow (AC)

---
+++ Ngay 2020-11-30/cow.cpp
@@ -0,0 +1,111 @@
+/* 𝙰𝚞𝚝𝚑𝚘𝚛: 𝙱𝚞𝚒 𝚅𝚒 𝚀𝚞𝚘𝚌 */
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=(a); i<=(b); i++)
+#define FORD(i, a, b) for(int i=(a); i>=(b); i--)
+#define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
+#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define testBit(n, bit) (((n) >> (bit)) & 1LL)
+#define flipBit(n, bit) ((n) ^ (1LL << (bit)))
+#define cntBit(n) __builtin_popcountll(n)
+#define sqr(x) ((x)*(x))
+#define endl '\n'
+// #define int long long
+using namespace std;
+
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
+}
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x > y + eps) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x + eps < y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+using ii = pair <int, int>;
+using ll = long long;
+using ld = long double;
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = { 0, 1, 0,-1};
+
+/*
+(\_/)
+( •_•)
+/ >?? */
+
+const int N = 100005;
+int n, k, a[N];
+
+#define FILE_IO
+signed main(void) {
+    FastIO;
+    #ifdef FILE_IO
+    freopen("cow.inp","r",stdin);
+    freopen("cow.out","w",stdout);
+    #endif
+
+    auto check = [&](const int &len) {
+        int cur = 1, p = 1;
+        while (cur < k) {
+            int pos = lower_bound(a + p, a + 1 + n, a[p] + len) - a;
+            if (pos > n) return false;
+            cur++; p = pos;
+        }
+        return cur >= k;
+    };
+
+    int T; Read(T);
+    while (T--) {
+        Read(n); Read(k);
+        FOR(i,1,n) Read(a[i]);
+        sort(a + 1, a + 1 + n);
+        int lo = 0, hi = 1e9, mi;
+        int res = 0;
+        while (lo <= hi) {
+            mi = lo + ((hi - lo) >> 1LL);
+            if (check(mi)) {
+                res = mi;
+                lo = mi + 1;
+            } else hi = mi - 1;
+        }
+        Write(res);
+        putchar(endl);
+    }
+    return 0;
+}
