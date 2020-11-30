6370750a18f78e50b688bbeb28e8e97344c6060a
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-11-30 22:03:51 +0700

    solve PARTY (AC)

---
+++ Ngay 2020-11-30/PARTY.cpp
@@ -0,0 +1,74 @@
+/* 𝙰𝚞𝚝𝚑𝚘𝚛: 𝙱𝚞𝚒 𝚅𝚒 𝚀𝚞𝚘𝚌 */
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define FORD(i, a, b) for(int i=a; i>=b; i--)
+#define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
+#define ALL(v) (v).begin(), (v).end()
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define testBit(n, bit) (((n) >> (bit)) & 1LL)
+#define flipBit(n, bit) ((n) ^ (1LL << (bit)))
+#define cntBit(n) __builtin_popcountll(n)
+#define sqr(x) ((x)*(x))
+#define endl '\n'
+// #define int long long
+using namespace std;
+
+template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x > y + eps) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x + eps < y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+using ii = pair <int, int>;
+using ll = long long;
+using ld = long double;
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = { 0, 1, 0,-1};
+
+/*
+(\_/)
+( •_•)
+/ >?? */
+
+int n, k, res;
+
+#define FILE_IO
+signed main(void) {
+    FastIO;
+    #ifdef FILE_IO
+    freopen("PARTY.INP","r",stdin);
+    freopen("PARTY.OUT","w",stdout);
+    #endif
+    cin >> n >> k;
+    if (n & 1) {
+        if (k + 1 >= n) res = n;
+        else res = k + 1;
+    } else {
+        int d = n / 2;
+        if (d % 2) d--;
+        if (k >= d) res = n >> 1;
+        else res = k + 1;
+    }
+
+    cout << res;
+    return 0;
+}
