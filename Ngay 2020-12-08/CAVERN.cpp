577d3bc6f849558e9dd7f15137dea5f88ca49a48
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-12-08 15:36:42 +0700

    CAVERN

--- Ngay 2020-12-08/CAVERN.cpp
+++ Ngay 2020-12-08/CAVERN.cpp
@@ -71,6 +71,21 @@ const int dy[] = { 0, 1, 0,-1};
 const int N = 3003;
 int n, m, res = 0;
 char a[N][N];
+int b[N][N], psum[N][N];
+
+bool isValid(const int &x, const int &y, const bool &checkInside) {
+    if (x < 1 || x > n) return false;
+    if (y < 1 || y > m) return false;
+    if (checkInside) return true;
+    return (a[x][y] == '.');
+}
+
+int sum(const int &x1, const int &y1, const int &x2, const int &y2) {
+    if (!isValid(x1, y1, 1) || !isValid(x2, y2, 1)) return false;
+    return psum[x2][y2] - psum[x1-1][y2] - psum[x2][y1-1] + psum[x1-1][y1-1];
+}
+
+bool ok;
 
 #define FILE_IO
 signed main(void) {
@@ -92,11 +107,51 @@ signed main(void) {
         int x; Read(x);
         FORD(i, n, n-x+1) a[i][j] = '#';
     }
+
+    // FOR(i,0,n+1) {
+    //     FOR(j,0,m+1) cout << a[i][j] << ' ';
+    //     cout << endl;
+    // }
+
+    FOR(i,0,n+1) FOR(j,0,m+1) if (a[i][j] == '#') b[i][j] = 1;
+    FOR(i,1,n) FOR(j,1,m) psum[i][j] = psum[i-1][j] + psum[i][j-1] - psum[i-1][j-1] + b[i][j];
     
+    // int lo = 1, hi = n;
+    // while (lo <= hi) {
+    //     int mi = lo + (hi - lo) / 2;
+    //     ok = true;
+    //     process(mi);
+    //     if (ok) {
+    //         lo = mi + 1;
+    //     } else hi = mi - 1;
+    // }
+
+    res = INT_MAX;
+
     FOR(i,0,n+1) {
         FOR(j,0,m+1) cout << a[i][j] << ' ';
         cout << endl;
     }
+    FOR(i,1,n) FOR(j,1,m) {
+        int cur = 0;
+        if (isValid(i, j, 0)) {
+            FORD(u,n,i) {
+                bool ok = false;
+                FORD(v,m,j) if (isValid(u, v, 0)) {
+                    if (sum(i,j,u,v) == 0) {
+                        maximize(cur, (u - i + 1)*(v - j + 1));
+                        cout << i << ' ' << j << ' ' << u << ' ' << v << endl;
+                        cout << sum(i,j,u,v) << endl;
+                        ok = true;
+                        break;
+                    }
+                }
+                if (ok) break;
+            }
+        }
+        if (cur) minimize(res, cur);
+    }
+    cout << res;
     
     // cerr << "\nExecution time: " << (double) clock() / 1000.0 << " second(s).";
     return 0;
