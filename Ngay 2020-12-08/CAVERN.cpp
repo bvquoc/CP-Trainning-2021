151a9e34615410e872cb49d5ae888ae9997d0e8d
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-12-08 15:00:01 +0700

    read input CAVERN

---
+++ Ngay 2020-12-08/CAVERN.cpp
@@ -0,0 +1,103 @@
+/* Author: Bui Vi Quoc */
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define FORD(i, a, b) for(int i=a; i>=b; i--)
+#define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
+#define ALL(v) (v).begin(), (v).end()
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define testBit(n, bit) (((n) >> (bit)) & 1LL)
+#define flipBit(n, bit) ((n) ^ (1LL << (bit)))
+#define cntBit(n) __builtin_popcountll(n)
+#define sqr(x) ((x)*(x))
+#define endl '\n'
+// #define int long long
+using namespace std;
+
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = (x << 3) + (x << 1) + (c - '0');
+    if (Neg) x = -x;
+}
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x > y + eps) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x + eps < y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+using ii = pair <int, int>;
+using ll = long long;
+using ld = long double;
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = { 0, 1, 0,-1};
+
+/*
+(\_/)
+( •_•)
+/ >?? */
+
+const int N = 3003;
+int n, m, res = 0;
+char a[N][N];
+
+#define FILE_IO
+signed main(void) {
+    FastIO;
+    #ifdef FILE_IO
+    freopen("CAVERN.INP","r",stdin);
+    freopen("CAVERN.OUT","w",stdout);
+    #endif
+    Read(n); Read(m);
+
+    memset(a, '#', sizeof a);
+    FOR(i,1,n) FOR(j,1,m) a[i][j] = '.';
+
+    FOR(j,1,m) {
+        int x; Read(x);
+        FOR(i,1,x) a[i][j] = '#';
+    }
+    FOR(j,1,m) {
+        int x; Read(x);
+        FORD(i, n, n-x+1) a[i][j] = '#';
+    }
+    
+    FOR(i,0,n+1) {
+        FOR(j,0,m+1) cout << a[i][j] << ' ';
+        cout << endl;
+    }
+    
+    // cerr << "\nExecution time: " << (double) clock() / 1000.0 << " second(s).";
+    return 0;
+}
