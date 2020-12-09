10460d9c7edde8fa00e7290063c1c0df4364e454
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-12-09 21:28:01 +0700

    solve POKEMON (subtask 1)

--- Ngay 2020-12-09_AM/POKEMON.cpp
+++ Ngay 2020-12-09_AM/POKEMON.cpp
@@ -17,6 +17,25 @@
 // #define int long long
 using namespace std;
 
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = (x << 3) + (x << 1) + (c - '0');
+    if (Neg) x = -x;
+}
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
 template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
 template<class X, class Y>
 bool minimize(X &x, const Y &y) {
@@ -46,6 +65,38 @@ using ld = long double;
 ( •_•)
 / >?? */
 
+const int N = 45;
+int n, m;
+struct Data { int x, y, t; } q[N];
+
+double res = DBL_MAX, cur = 0.0;
+int order[N]; bool mark[N];
+double dist(const Data &A, const Data &B) {
+    ll tmp = 1LL * sqr(A.x - B.x) + sqr(A.y - B.y);
+    return sqrt(tmp);
+}
+double d[N][N];
+
+void Try(int id) {
+    if (id > n) {
+        // FOR(i,1,n) cout << order[i] << ' ';
+        // cout << cur << endl;
+        res = cur;
+        return;
+    }
+
+    FOR(i,1,n) if (!mark[i]) {
+        double tmp = d[order[id-1]][i];
+        if (cur + tmp > res) continue;
+        order[id] = i;
+        mark[i] = true;
+        cur += tmp;
+        Try(id+1);
+        cur -= tmp;
+        mark[i] = false;
+    }
+}
+
 #define FILE_IO
 signed main(void) {
     FastIO;
@@ -53,7 +104,20 @@ signed main(void) {
     freopen("POKEMON.INP","r",stdin);
     freopen("POKEMON.OUT","w",stdout);
     #endif
-    
+    Read(n); Read(m);
+    q[0] = {0,0,0};
+    FOR(i,1,n) {
+        Read(q[i].x);
+        Read(q[i].y);
+        Read(q[i].t);
+    }
+
+    order[0] = 0;
+    FOR(i,0,n) FOR(j,0,n) d[i][j] = dist(q[i], q[j]);
+    Try(1);
+    cout << fixed << setprecision(3) << res;
+
+    #define subtask_1 (n <= 17 && m == n)
     // cerr << "\nExecution time: " << (double) clock() / 1000.0 << " second(s).";
     return 0;
 }
