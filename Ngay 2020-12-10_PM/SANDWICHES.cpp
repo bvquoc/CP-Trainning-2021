6c00dbe92dc745294b939c15d23d105f6dd0151c
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-12-10 14:48:43 +0700

    solve SANDWICHES (dp)

---
+++ Ngay 2020-12-10_PM/SANDWICHES.cpp
@@ -0,0 +1,108 @@
+/* Author: Bui Vi Quoc */
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define fi first
+#define se second
+#define FOR(i, a, b) for(int i=(a); i<=(b); i++)
+#define FORD(i, a, b) for(int i=(a); i>=(b); i--)
+#define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
+#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define sz(a) (int(a.size()))
+#define BIT(a, i) (((a) >> (i)) & 1LL)
+#define MASK(i) (1LL << (i))
+#define turnON(a, i) ((a) | MASK(i))
+#define turnOFF(a, i) ((a) & (~MASK(i)))
+#define flipBit(n, bit) ((n) ^ (1LL << (bit)))
+#define cntBit(n) __builtin_popcountll(n)
+#define sqr(x) ((x)*(x))
+#define endl '\n'
+#define int long long
+using namespace std;
+
+template <typename T>
+inline void Read(T& x) {
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' || c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = (x << 3) + (x << 1) + (c - '0');
+    if (Neg) x = -x;
+}
+template <typename T>
+inline void Write(T x) {
+    if (x < 0) { putchar('-'); x = -x; }
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+template<class T> T Abs(const T &x) { return (x < 0 ? -x : x); }
+template<class X, class Y>
+bool minimize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x > y + eps) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+template<class X, class Y>
+bool maximize(X &x, const Y &y) {
+    X eps = 1e-9;
+    if (x + eps < y) {
+        x = y;
+        return true;
+    }
+    return false;
+}
+
+using ii = pair <int, int>;
+using ll = long long;
+using ld = long double;
+
+/*
+(\_/)
+( •_•)
+/ >?? */
+
+const int N = 502, INF = 1e12 + 7;
+int n, k, a[N];
+
+int f[N][3*N];
+int dp(const int n, const int k) {
+    if (n == 0 && k == 0) return 0;
+    if (n * k == 0) return -INF;
+    if (k < n || k > 2 * n) return -INF;
+    if (f[n][k] != -1) return f[n][k];
+
+    int &ans = f[n][k];
+    FOR(i,1,n) maximize(ans, a[i] + max(dp(n - i, k - i), dp(n - i, k - i - 1)));
+    return ans;
+}
+
+#define FILE_IO
+signed main(void) {
+    FastIO;
+    #ifdef FILE_IO
+    freopen("SANDWICHES.INP","r",stdin);
+    freopen("SANDWICHES.OUT","w",stdout);
+    #endif
+    Read(n); Read(k);
+    FOR(i,1,n) Read(a[i]);
+
+    #define noSol (k < n || k > 2 * n)
+    if (noSol) {
+        cout << "Impossible";
+        exit(0);
+    }
+
+    memset(f, -1LL, sizeof f);
+    int res = dp(n, k);
+    if (res <= -INF) cout << "Impossible";
+    else Write(res);
+    // cerr << "\nExecution time: " << (double) clock() / 1000.0 << " second(s).";
+    return 0;
+}
