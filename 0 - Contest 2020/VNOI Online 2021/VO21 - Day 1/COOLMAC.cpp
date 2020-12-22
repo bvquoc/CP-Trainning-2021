035694bc0460d273c673f8202063d9361f986927
bvquoc2003 <quocpro10.02.2003@gmail.com>
2020-12-22 09:45:19 +0700

    solve COOLMAC (AC)

--- 0 - Contest 2020/VNOI Online 2021/VO21 - Day 1/COOLMAC.cpp
+++ 0 - Contest 2020/VNOI Online 2021/VO21 - Day 1/COOLMAC.cpp
@@ -1,8 +1,8 @@
 /* Author: Bui Vi Quoc */
 #include <bits/stdc++.h>
 #define FastIO ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-#define fi first
-#define se second
+#define L first
+#define R second
 #define FOR(i, a, b) for(int i=(a); i<=(b); i++)
 #define FORD(i, a, b) for(int i=(a); i>=(b); i--)
 #define REP(i, n) for(int i=0, _n=(n); i<_n; i++)
@@ -67,16 +67,38 @@ using ld = long double;
 (\_/)
 ( •_•)
 / >?? */
-
-const int N = 1000005, INF = 2e9, LIM = 100000;
-int m, n, a[N];
-struct Clo { 
-    int L, R; 
-    bool operator< (const Clo &B) {
-        return L < B.L || (L == B.L && R > B.R);
+const int N = 1000005, INF = 2e9, LIM = 100001;
+struct FenwickTree {
+    vector<int> bit;
+    int n;
+    FenwickTree() {}
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n+1, INF);
     }
-} b[N];
-int res = INF;
+    void assign (int n) {
+        this->n = n;
+        bit.assign(n+1, INF);
+    }
+
+    FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++) update(i, a[i]);
+    }
+
+    int getMin(int r) {
+        int res = INF;
+        for (; r >= 1; r = (r & (r + 1)) - 1) res = min(res, bit[r]);
+        return res;
+    }
+
+    void update(int idx, int val) {
+        for (; idx <= n; idx = idx | (idx + 1)) 
+            bit[idx] = min(bit[idx], val);
+    }
+} BIT;
+
+int m, n;
+ii a[N];
 
 #define FILE_IO
 signed main(void) {
@@ -87,59 +109,30 @@ signed main(void) {
     #endif
     
     Read(m);
-    FOR(i,1,m) Read(b[i].L), Read(b[i].R);
+    FOR(i,1,m) {
+        Read(a[i].L), Read(a[i].R);
+        a[i].L++; a[i].R++;
+    }
+
+    sort(a + 1, a + 1 + m, greater <ii>());
+
+    BIT.assign(LIM);
+    FOR(i,1,m) {
+        int ans = N;
+        if (a[i].R == LIM) ans = 1;
+        else minimize(ans, BIT.getMin(a[i].R)+1);
+        BIT.update(a[i].L, ans);
+    }
+
+    int minT = INF;
     Read(n);
-    FOR(i,1,n) Read(a[i]);
-
-    sort(a + 1, a + 1 + n);
-    sort(b + 1, b + 1 + m);
-
-    #define subtask_1 (n <= 10 && m <= 16)
-
-    // if (subtask_1) {
-    //     vector <int> d;
-    //     REP(mask, MASK(m)) {
-    //         d.clear();
-    //         REP(i, m) if (BIT(mask, i)) d.emplace_back(i+1);
-    //         int L = a[1];
-    //         for(int i: d) {
-    //             if (b[i].L <= L && L <= b[i].R) {
-    //                 L = b[i].R;
-    //             }
-    //         }
-
-    //         if (L >= LIM) minimize(res, d.size());
-    //     }
-
-    //     if (res == INF) res = -1;
-    //     Write(res);
-    //     exit(0);
-    // }
-
-    int mx_R = INT_MIN, mn_L = b[1].L;
-    FOR(i, 1, m) maximize(mx_R, b[i].R);
-
-    if (mx_R < LIM || mn_L > a[1]) {
-        putchar('-');
-        putchar('1');
-        exit(0);
+    FOR(i,1,n) {
+        int x; Read(x); x++;
+        minimize(minT, x);
     }
 
-    vector <int> d;
-    REP(mask, MASK(m)) {
-        d.clear();
-        REP(i, m) if (BIT(mask, i)) d.emplace_back(i+1);
-        int L = a[1];
-        for(int i: d) {
-            if (b[i].L <= L && L <= b[i].R) {
-                L = b[i].R;
-            }
-        }
-
-        if (L >= LIM) minimize(res, d.size());
-    }
-
-    if (res == INF) res = -1;
+    int res = BIT.getMin(minT);
+    if (res > LIM) res = -1;
     Write(res);
     exit(0);
 
